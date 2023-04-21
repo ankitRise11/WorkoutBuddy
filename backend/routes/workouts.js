6358392f6ef198fdf12a7e1cd88c8ceec7c6dd22
Ankit Mahotla <ankit@rise11.com>
2023-04-21 13:22:05 +0530

    Added Controllers

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,34 +1,17 @@
 const express = require("express");
+const {
+  createWorkout,
+  getWorkout,
+  getWorkouts,
+  updateWorkout,
+  deleteWorkout,
+} = require("../controllers/workoutController");
 const router = express.Router();
-const Workout = require("../models/workoutModel");
 
-router.get("/", (req, res) => {
-  res.json({ message: "GET all workouts" });
-});
-router.get("/:id", (req, res) => {
-  res.json({ message: "GET a single workout" });
-});
-router.post("/", async (req, res) => {
-  const { title, reps, load } = req.body;
-  // if (!title || !reps || !load) {
-  //   return res.status(400).json({ error: "Missing required fields" });
-  // }
-  try {
-    const workout = await Workout.create({
-      title,
-      reps,
-      load,
-    });
-    res.status(200).json(workout);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-});
-router.patch("/:id", (req, res) => {
-  res.json({ message: "UPDATE a workout" });
-});
-router.delete("/:id", (req, res) => {
-  res.json({ message: "DELETE a workout" });
-});
+router.get("/", getWorkouts);
+router.get("/:id", getWorkout);
+router.post("/", createWorkout);
+router.patch("/:id", updateWorkout);
+router.delete("/:id", deleteWorkout);
 
 module.exports = router;
