3bc7a4ed4309571f02d5d52f5d8362a365e441ba
Ankit Mahotla <ankit@rise11.com>
2023-04-24 12:02:48 +0530

    Handled error and final touches

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -21,6 +21,22 @@ const getWorkout = async (req, res) => {
 
 const createWorkout = async (req, res) => {
   const { title, reps, load } = req.body;
+  let emptyFields = [];
+  if (!title) {
+    emptyFields.push("title");
+  }
+  if (!load) {
+    emptyFields.push("load");
+  }
+  if (!reps) {
+    emptyFields.push("reps");
+  }
+  if (emptyFields.length > 0) {
+    return res
+      .status(400)
+      .json({ error: "Please fill in all the fields", emptyFields });
+  }
+
   // if (!title || !reps || !load) {
   //   return res.status(400).json({ error: "Missing required fields" });
   // }
