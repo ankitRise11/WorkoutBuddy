921e6f4cffbad90e8f42f8748f26bdd6ad9cfc9c
ankitmahotla <ankitmahotla45@gmail.com>
2023-04-27 21:27:44 +0530

    Added jwt to controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,9 @@
 const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
+
+const createToken = (_id) => {
+  return jwt.sign({ _id }, process.env.SECRET, { expiresIn: "3d" });
+};
 
 const loginUser = async (req, res) => {
   res.json({ mssg: "Login User" });
@@ -9,12 +14,14 @@ const signupUser = async (req, res) => {
   try {
     const user = await User.signup(email, password);
 
+    const token = createToken(user._id);
+
     // check if the user is an instance of the Error object
     if (user instanceof Error) {
       return res.status(400).json({ error: user.message });
     }
 
-    res.status(200).json({ email, user });
+    res.status(200).json({ email, token });
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
