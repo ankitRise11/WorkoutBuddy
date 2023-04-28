68d4a14298d5d78a3751c028d7cbd7bb63a4c620
Ankit Mahotla <ankit@rise11.com>
2023-04-28 16:54:29 +0530

    Added Signup functionality in frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
 import UpdateData from "./pages/UpdateData";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 function App() {
   return (
     <>
@@ -11,6 +13,8 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/update-data/:id" element={<UpdateData />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
           </Routes>
         </div>
       </BrowserRouter>
