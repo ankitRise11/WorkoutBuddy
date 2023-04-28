ca9b286e9afa612abccef661b61cbeda1a22a6b0
Ankit Mahotla <ankit@rise11.com>
2023-04-28 12:23:49 +0530

    Backend for Authentication Completed

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -6,7 +6,21 @@ const createToken = (_id) => {
 };
 
 const loginUser = async (req, res) => {
-  res.json({ mssg: "Login User" });
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.login(email, password);
+
+    const token = createToken(user._id);
+
+    if (user instanceof Error) {
+      return res.status(400).json({ error: user.message });
+    }
+
+    res.status(200).json({ email, token });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 };
 
 const signupUser = async (req, res) => {
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const brcypt = require("bcrypt");
+const bcrypt = require("bcrypt");
 const validator = require("validator");
 const Schema = mongoose.Schema;
 
@@ -34,11 +34,30 @@ userSchema.statics.signup = async function (email, password) {
     return new Error("Email already in use");
   }
 
-  const salt = await brcypt.genSalt(10);
-  const hash = await brcypt.hash(password, salt);
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(password, salt);
 
   const user = await this.create({ email, password: hash });
 
+  return user;
+};
+
+userSchema.statics.login = async function (email, password) {
+  if (!email || !password) {
+    return new Error("All fields must be filled");
+  }
+
+  const user = await this.findOne({ email });
+
+  if (!user) {
+    return new Error("Incorrect email");
+  }
+
+  const match = await bcrypt.compare(password, user.password);
+
+  if (!match) {
+    return new Error("Incorrect Password");
+  }
   return user;
 };
 
